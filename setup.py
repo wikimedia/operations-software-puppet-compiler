30f1527d07069cf8d9de3fd38fd0501da56f5bc5
elukey <ltoscano@wikimedia.org>
2016-04-14 13:46:35 +0200

    Bumping version to 0.1.3
    Bug: T132154
    Change-Id: Ia631620f2dddcc08c48236fd6fa4a15f3be8347b

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def all_files(cwd, path):
 
 setup(
     name='puppet_compiler',
-    version='0.1.2',
+    version='0.1.3',
     description='Tools to compile puppet catalogs as a service',
     author='Joe',
     author_email='glavagetto@wikimedia.org',
