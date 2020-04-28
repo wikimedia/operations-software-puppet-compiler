eb48b29346f852a65ee82713c72164bd2ad45aa6
John Bond <jbond@wikimedia.org>
2020-04-28 10:45:43 +0200

    0.7.3: prepare release
    Change-Id: Ibaf13d43caf036b1c1b380d4a33688776c8b226b

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
-from functools import reduce
 import os
-from setuptools import setup, find_packages
+
+from functools import reduce
+
+from setuptools import find_packages, setup
 
 
 def all_files(cwd, path):
@@ -12,7 +14,7 @@ def all_files(cwd, path):
 
 setup(
     name='puppet_compiler',
-    version='0.7.2',
+    version='0.7.3',
     description='Tools to compile puppet catalogs as a service',
     author='Joe',
     author_email='glavagetto@wikimedia.org',
