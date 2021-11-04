9542b63985156b26f731a152750b3f3d2f6fc7ce
jbond <jbond@wikimedia.org>
2021-11-04 13:45:18 +0100

    populate_puppetdb: add support for cloud nodes
    Change-Id: Id4b7a5a5a770a93243af9c250cacdf7edc8eb5f9

--- puppet_compiler/populate_puppetdb.py
+++ puppet_compiler/populate_puppetdb.py
@@ -1,4 +1,4 @@
-from __future__ import print_function
+#!/usr/bin/python3
 
 import argparse
 import logging
@@ -8,61 +8,85 @@ import tempfile
 
 from puppet_compiler import directories, nodegen, prepare, puppet, utils
 
-parser = argparse.ArgumentParser(
-    description="Puppetdb filler - this script allows to properly populate PuppetDB with data useful for the puppet compiler"
-)
-parser.add_argument(
-    "--basedir",
-    default="/var/lib/catalog-differ",
-    help="The base dir of the compiler installation",
-)
-parser.add_argument("--debug", action="store_true", default=False, help="Print debug output")
-parser.add_argument("--host", help="if present only populate the DB for this host")
+
+def get_args():
+    """Get Arguments"""
+    parser = argparse.ArgumentParser(
+        description=(
+            "Puppetdb filler - this script allows to properly populate "
+            "PuppetDB with data useful for the puppet compiler"
+        )
+    )
+    parser.add_argument(
+        "--basedir",
+        default="/var/lib/catalog-differ",
+        help="The base dir of the compiler installation",
+    )
+    parser.add_argument("--debug", action="store_true", default=False, help="Print debug output")
+    parser.add_argument("--host", help="if present only populate the DB for this host")
+    return parser.parse_args()
+
+
+def populate_node(node, config):
+    """populate puppetdb for this specific node"""
+    print("=" * 80)
+    print("Compiling catalog for {}".format(node))
+
+    try:
+        utils.refresh_yaml_date(utils.facts_file(config["puppet_var"], node))
+    except utils.FactsFileNotFound as error:
+        print("ERROR: {}".format(error))
+        return
+    for manifest_dir in ["/dev/null", None]:
+        print("manifest_dir: {}".format(manifest_dir))
+        succ, _, err = puppet.compile_storeconfigs(node, config["puppet_var"], manifest_dir)
+        if succ:
+            print("OK")
+        else:
+            for line in err:
+                print(line)
 
 
 def main():
-    opts = parser.parse_args()
-    if opts.debug:
+    """main entry"""
+    args = get_args()
+    if args.debug:
         lvl = logging.DEBUG
     else:
         lvl = logging.INFO
 
-    logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s", level=lvl, datefmt="[ %Y-%m-%dT%H:%M:%S ]")
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)s: %(message)s",
+        level=lvl,
+        datefmt="[ %Y-%m-%dT%H:%M:%S ]",
+    )
 
     config = {
-        "puppet_var": os.path.join(opts.basedir, "puppet"),
-        "puppet_src": os.path.join(opts.basedir, "production"),
-        "puppet_private": os.path.join(opts.basedir, "private"),
+        "puppet_var": os.path.join(args.basedir, "puppet"),
+        "puppet_src": os.path.join(args.basedir, "production"),
+        "puppet_private": os.path.join(args.basedir, "private"),
     }
     # Do the whole compilation in a dedicated directory.
     tmpdir = tempfile.mkdtemp(prefix="fill-puppetdb")
     jobid = "1"
     directories.FHS.setup(jobid, tmpdir)
-    m = prepare.ManageCode(config, jobid, None)
-    os.mkdir(m.base_dir, 0o755)
-    os.makedirs(m.prod_dir, 0o755)
-    m._prepare_dir(m.prod_dir)
-    srcdir = os.path.join(m.prod_dir, "src")
+    managecode = prepare.ManageCode(config, jobid, None)
+    os.mkdir(managecode.base_dir, 0o755)
+    os.makedirs(managecode.prod_dir, 0o755)
+    managecode._prepare_dir(managecode.prod_dir)
+    srcdir = os.path.join(managecode.prod_dir, "src")
+    nodes = set([args.host]) if args.host else nodegen.get_nodes(config)
+    cloud_nodes = set(n for n in nodes if n.endswith(("wikimedia.cloud", "wmflabs")))
+    prod_nodes = nodes - cloud_nodes
     with prepare.pushd(srcdir):
-        m._copy_hiera(m.prod_dir, "production")
-    nodes = [opts.host] if opts.host else nodegen.get_nodes(config)
-    for node in nodes:
-        print("=" * 80)
-        print("Compiling catalog for {}".format(node))
-
-        try:
-            utils.refresh_yaml_date(utils.facts_file(config["puppet_var"], node))
-        except utils.FactsFileNotFound as error:
-            print("ERROR: {}".format(error))
-            continue
-        for manifest_dir in ["/dev/null", None]:
-            print("manifest_dir: {}".format(manifest_dir))
-            succ, out, err = puppet.compile_storeconfigs(node, config["puppet_var"], manifest_dir)
-            if succ:
-                print("OK")
-            else:
-                for line in err:
-                    print(line)
+        print(f"{30 * '#'} working on {len(prod_nodes)} prod nodes {30 * '#'}")
+        managecode._copy_hiera(managecode.prod_dir, "production")
+        for node in prod_nodes:
+            populate_node(node, config)
+        print(f"{30 * '#'} working on {len(cloud_nodes)} cloud nodes {30 * '#'}")
+        managecode._copy_hiera(managecode.prod_dir, "labs")
+        for node in cloud_nodes:
+            populate_node(node, config)
     shutil.rmtree(tmpdir)
 
 
