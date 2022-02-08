302bd92336a90f4febfbf7acfb7009da1731f1e0
John Bond <jbond@wikimedia.org>
2022-02-08 22:25:09 +0100

    2.1.0: prepare for release
    Change-Id: I96d253cd8461f7ce13c48c990cf5da93dc506368

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ def get_templates() -> List[str]:
 
 setup(
     name="puppet_compiler",
-    version="2.0.1",
+    version="2.1.0",
     description="Tools to compile puppet catalogs as a service",
     author="Joe",
     author_email="glavagetto@wikimedia.org",
