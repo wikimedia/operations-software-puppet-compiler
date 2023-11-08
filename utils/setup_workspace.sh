21d1fb2b52b2fd6faaebfe804c0aa1ed7e6a8336
jbond <jbond@wikimedia.org>
2023-11-08 11:58:18 +0100

    utils/setup_workspace.sh: update setup options
    Change-Id: Idd90d0ee11c24d55020a425cee3e72669dd238bb

--- utils/setup_workspace.sh
+++ utils/setup_workspace.sh
@@ -27,20 +27,22 @@ main() {
     local workspace_path="${1:-.workspace}"
     local prod_puppet_git_path="${workspace_path}/catalog-differ/production"
     local private_git_path="${workspace_path}/catalog-differ/private"
+    local netbox_git_path="${workspace_path}/catalog-differ/netbox-hiera"
 
     mkdir -p "${workspace_path}"/catalog-differ/{production,private,puppet/ssl} "${workspace_path}/jenkins-workspace"
 
-    setup_repo "$prod_puppet_git_path" "https://gerrit.wikimedia.org/r/operations/puppet" 
-    setup_repo "$private_git_path" "https://gerrit.wikimedia.org/r/labs/private" 
+    setup_repo "$prod_puppet_git_path" "https://gerrit.wikimedia.org/r/operations/puppet"
+    setup_repo "$private_git_path" "https://gerrit.wikimedia.org/r/labs/private"
+    setup_repo "$netbox_git_path" "https://netbox-exports.wikimedia.org/netbox-hiera"
 
-    puppet master \
-        --compile test \
+    puppet catalog \
+        compile test \
         --vardir  "${workspace_path}/catalog-differ/puppet"
 
-    puppet cert \
-        --ssldir  "${workspace_path}/catalog-differ/puppet/ssl" \
-        --vardir "${workspace_path}/catalog-differ/puppet" \
-        generate $(hostname -f)
+    #puppet cert \
+    #    --ssldir  "${workspace_path}/catalog-differ/puppet/ssl" \
+    #    --vardir "${workspace_path}/catalog-differ/puppet" \
+    #    generate $(hostname -f)
 }
 
 
